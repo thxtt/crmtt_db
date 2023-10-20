cfcc2249d9555c4980f9993de989553e9f9fec22
Bohdan <134650060+thxtt@users.noreply.github.com>
2023-10-20 19:13:00 +0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,92 @@
+const db = [
+    {
+        userId:1,
+        id: 1,
+        name: 'Филипп Радушко',
+        tel: '380631111111',
+        email: 'philop1@gmail.com',
+        region: 'Страна, Город',
+        slack: 'QWERTY1'
+    },
+    {
+        userId:1,
+        id: 2,
+        name: 'Андрей Бочаров',
+        tel: '380632222222',
+        email: 'andreev2@gmail.com',
+        region: 'Страна, Город',
+        slack: 'QWERTY2'
+    },
+    {
+        userId:1,
+        id: 3,
+        name: 'Инна Гутор',
+        tel: '380633333333',
+        email: '',
+        region: 'Страна, Город',
+        slack: 'QWERTY3'
+    },
+    {
+        userId:1,
+        id: 4,
+        name: 'Ваня Петренко',
+        tel: '380634444444',
+        email: 'vanya4@gmail.com',
+        region: '',
+        slack: 'QWERTY4'
+    },
+    {
+        userId:1,
+        id: 5,
+        name: 'Алексей Флерко',
+        tel: '380635555555',
+        email: 'alexey5@gmail.com',
+        region: 'Страна, Город',
+        slack: ''
+    },
+    {
+        userId:1,
+        id: 6,
+        name: 'Рамиль Фаридов',
+        tel: '380636666666',
+        email: 'etonepochta6@gmail.com',
+        region: 'Страна, Город',
+        slack: 'QWERTY6'
+    },
+    {
+        userId:1,
+        id: 7,
+        name: 'Елизавета Рачко',
+        tel: '380637777777',
+        email: 'elizaveta7@gmail.com',
+        region: 'Страна, Город',
+        slack: 'QWERTY7'
+    },
+    {
+        userId:1,
+        id: 8,
+        name: 'Сергей Фломов',
+        tel: '380638888888',
+        email: 'flomov8@gmail.com',
+        region: 'Страна, Город',
+        slack: 'QWERTY8'
+    },
+    {
+        userId:1,
+        id: 9,
+        name: 'Антон Верев',
+        tel: '380639999999',
+        email: 'anton9@gmail.com',
+        region: 'Страна, Город',
+        slack: 'QWERTY9'
+    },
+    {
+        userId:1,
+        id: 10,
+        name: 'Федя Попов',
+        tel: '380630000000',
+        email: 'popov0@gmail.com',
+        region: 'Страна, Город',
+        slack: 'QWERTY0'
+    },
+]
